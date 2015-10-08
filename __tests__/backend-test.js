fb5cc8d9fb4a6e02175000cfc3f7efda946ab943
Michael Gao <mgao4793@gmail.com>
2015-10-08 00:01:34 -0400

    Add tests

---
+++ __tests__/backend-test.js
@@ -0,0 +1,48 @@
+describe('backend', () => {
+  describe('login', () => {
+  });
+
+  describe('crud', () => {
+    it('clears a sheet', () => {
+    });
+
+    it('creates a new sheet', () => {
+    });
+
+    it('creates a new workbook', () => {
+    });
+
+    it('gets cells', () => {
+    });
+  });
+
+  describe('eval', () => {
+    describe('initial eval', () => {
+    });
+
+    describe('repl eval', () => {
+    });
+
+    describe('update propagation', () => {
+    });
+
+    describe('dag updates', () => {
+    });
+  });
+
+  describe('cell transforms', () => {
+    describe('cut/paste', () => {
+    });
+
+    describe('copy/paste', () => {
+    });
+  });
+
+  describe('vcs', () => {
+    describe('undo', () => {
+    });
+
+    describe('redo', () => [
+    ]);
+  });
+});
