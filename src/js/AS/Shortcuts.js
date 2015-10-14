43a839c5a83e3cfb93f35f1654dafbc296ae2f4e
Timothy Chu <timchu@mit.edu>
2015-10-14 03:24:27 -0400

    Added a Ctrl+Shift+Enter shortcut to add  {}s around Excel formulas.

--- src/js/AS/Shortcuts.js
+++ src/js/AS/Shortcuts.js
@@ -17,12 +17,29 @@ export default {
       // TODO
     });
     ShortcutUtils.addShortcut("common", "cell_eval", ["Ctrl+Enter", "Command+Enter", "Ctrl+D", "Ctrl+R"], (wildcard) => {
+      console.log("BOOGA");
       let editorState = {
         exp: self._getRawEditor().getValue(),
         lang: self.state.language
       };
       self.handleEvalRequest(editorState);
     });
+    ShortcutUtils.addShortcut("common", "cell_eval", ["Ctrl+Shift+Enter", "Command+Shift+Enter"], (wildcard) => {
+      console.log("What's up dawg");
+      var editorValue = self._getRawEditor().getValue();
+      console.log(self.state.language);
+      if (self.state.language == Constants.Languages.Excel){
+        console.log("In loop");
+        editorValue = "{" + self._getRawEditor().getValue() + "}";
+        self._getRawEditor().setValue(editorValue);
+      }
+      let editorState = {
+        exp: editorValue,
+        lang: self.state.language
+      };
+      self.handleEvalRequest(editorState);
+    });
+
     ShortcutUtils.addShortcut("common", "set_language", ["Ctrl+1/2/3/4/5/6/7/8/9", "Command+1/2/3/4/5/6/7/8/9"], (wildcard) => {
       // TODO propagate dropdown
       switch(wildcard) {
