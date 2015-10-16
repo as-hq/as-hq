778089166e088aa0b708c4afb7cc443b4ff78272
Alex <zhukeepa@gmail.com>
2015-10-15 23:33:59 -0400

    list eval dependencies fixed

--- server/src/AS/Dispatch/Core.hs
+++ server/src/AS/Dispatch/Core.hs
@@ -6,6 +6,7 @@ import AS.Types.DB
 import Prelude 
 import qualified AS.Eval.Core as EC (evaluateLanguage)
 import qualified Data.Map   as M
+import qualified Data.Set   as S
 import qualified AS.DB.API  as DB
 import qualified AS.DB.Util as DU
 import qualified Data.List  as L (head,last,tail,length,splitAt) 
@@ -110,20 +111,50 @@ getValuesMap locs = do
 -- if a cell references an ancestor, that ancestor is guaranteed to already have been 
 -- added in the map. 
 evalChain :: Connection -> RefValMap -> [ASCell] -> EitherTExec ([ASCell], [ASList])
-evalChain _ _ [] = return ([], [])
-evalChain conn valuesMap (c@(Cell loc xp _ ts):cs) = do  
+evalChain conn valuesMap cells = evalChain' conn valuesMap cells [] []
+
+
+-- | evalChain' works in two parts. First, it goes through the list of cells passed in and
+-- evaluates them. Along the way, new list cells (created as part of a list) are created 
+-- that may need to get re-evaluated. These get recorded in the fourth argument (type [ASList]), 
+-- and the heads of these lists get recorded in the fifth. 
+-- 
+-- When we finish evaluating the original list of cells, we go through the newly created list cells
+-- and essentially re-evaluate those. We are NOT just running eval on this list of cells directly, 
+-- because we don't need to re-evaluate the individual cells in the list, ONLY their descendants. 
+-- We also need to check for circular dependencies, which is why the pastListHeads are passed in. 
+-- #needsrefactor there's probably a more Haskell way of doing this with a state monad or something.
+evalChain' :: Connection -> RefValMap -> [ASCell] -> [ASList] -> [ASIndex] -> EitherTExec ([ASCell], [ASList])
+evalChain' _ _ [] [] _ = return ([], [])
+evalChain' conn valuesMap [] lists pastListHeads = do 
+  -- get cells from lists
+  let cells         = concat $ map snd lists
+      listCellLocs  = map cellLocation cells
+      listCellLocs' = filter (\d -> not $ d `elem` pastListHeads) listCellLocs
+  descs <- G.getDescendants listCellLocs'
+  -- check for circular dependencies. IF a circular dependency exists, it necessarily has to 
+  -- involve one of the list heads, since the cells created as part of a list depend only
+  -- on the head. So we go through the descendants of the current list cells (sans the previous
+  -- list heads), so if those contain any of the previous list heads we know there's a cycle. 
+  mapM_ (\d -> if (d `elem` pastListHeads) then (left $ CircularDepError d) else (return ())) descs
+  -- DON'T re-eval anything in the current set of list cells
+  let descs' = filter (\d -> not $ d `elem` listCellLocs) descs
+  cells' <- getCellsToEval conn descs' [] -- the origCells are the list cells, which got filtered out of descs'
+  evalChain' conn valuesMap cells' [] pastListHeads
+
+evalChain' conn valuesMap (c@(Cell loc xp _ ts):cs) next listHeads = do 
   cv <- EC.evaluateLanguage (IndexRef (cellLocation c)) (locSheetId loc) valuesMap xp
   case cv of 
     ValueL lst -> do
       let cellsList = createListCells conn c lst
-          newMp     = M.insert (IndexRef loc) (head lst) valuesMap
-      (restCells, restLists) <- evalChain conn newMp cs
+          newMp     = foldr (\(Cell l _ v _) mp -> M.insert (IndexRef l) v mp) valuesMap (snd cellsList)
+          -- ^ adds all the cells in cellsList 
+      (restCells, restLists) <- evalChain' conn newMp cs (cellsList:next) (loc:listHeads)
       return (restCells, cellsList:restLists)
     _ -> do
       let newValuesMap = M.insert (IndexRef loc) cv valuesMap
-      (restCells, restLists) <- evalChain conn newValuesMap cs
+      (restCells, restLists) <- evalChain' conn newValuesMap cs next listHeads
       return $ ((Cell loc xp cv ts):restCells, restLists)
--- TODO: The Haskell way is probably to write this using foldM somehow, but that's not very urgent. 
 
 -- | If a cell C contains a 1D or 2D list, it'll be represented in the grid as a matrix. 
 -- This function takes in the starting cell with the starting expression, and creates the list 
