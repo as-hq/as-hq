e2caf695273dc469aa831280eb6accfd0edfb3d1
Alex <zhukeepa@gmail.com>
2015-10-19 20:53:53 -0400

    uppercase boolean values

--- src/js/AS/Util.js
+++ src/js/AS/Util.js
@@ -37,7 +37,7 @@ export default {
       case "ValueNaN":
         return "NaN";
       case "ValueB":
-        return cv.contents;
+        return cv.contents.toString().toUpperCase();
       case "ValueD":
         return cv.contents;
       case "ValueI":
