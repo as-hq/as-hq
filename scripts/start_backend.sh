0bc73ac48438a634d16ef72b69a1c4978206d5c4
anand <asrini@mit.edu>
2015-11-06 19:34:01 -0500

    small fix to script path

--- scripts/start_backend.sh
+++ scripts/start_backend.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd server
+cd ../server
 until .stack-work/install/x86_64-linux/lts-3.7/7.10.2/bin/alphasheets-exe; do
     echo "Backend server crashed with exit code $?. Respawning..." >&2
     sleep 1
