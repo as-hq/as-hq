1766a4355b0fe21eb645d9caa9cf84ff578d87eb
anand <asrini@mit.edu>
2015-11-06 19:54:46 -0500

    sigkill propagates to parent process in persistent script now

--- scripts/start_graphdb.sh
+++ scripts/start_graphdb.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 until ../graph-database/as-graph-server; do
+    if [ $? -eq 137 ]
+        then exit
+    fi
 	echo "Graph database crashed with exit code $?. Respawning..." >&2
 	sleep 1
 done
