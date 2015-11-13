63d929ce103d0b98781c3510423d650178336828
RiteshRN <riteshr@mit.edu>
2015-11-13 15:25:58 -0500

    backend tag changes for image

---
+++ server/eval_files/sql/run.py
@@ -0,0 +1,66 @@
+#ADD COMMANDS HERE
+#from AS.stdlib import *
+from AS.ui.styling import *
+# from AS.tests.min import *
+from AS.instruments.ETF import ETF
+from AS.instruments.Stock import Stock
+# from AS.ui.plot import *
+import json
+from sys import exc_info
+import traceback
+from AS.pandas.input import listToDataframe
+from AS.pandas.output import pprint
+from pandasql import sqldf
+import pandas as pd
+
+from sqlalchemy import create_engine
+from pandas import read_sql_query
+
+pysqldf = lambda q: sqldf(q, globals())
+
+result = "DefaultSqlValue"
+def db(dbCmd,dbName=""):
+	try:
+		if "replaceCmd" in globals():
+			dbCmd = replaceCmd(dbCmd)
+		return pysqldf(dbCmd).head()
+	except:
+		try:
+			e = create_engine('sqlite:///'+dbName) #absolute file path
+			return read_sql_query(dbCmd,e)
+		except Exception as e: 
+			return pprintErr(e)
+
+def modifyReplace(i,e):
+	if "replaceCmd" in globals():
+		return lambda s : replaceCmd(s).replace("dataset"+str(i),e)
+	else:
+		return lambda s : s.replace("dataset"+str(i),e)
+
+
+def setGlobals(context):
+	for i in range(len(context)):
+		e = eval(context[i])
+		if isinstance(e,list):
+			globals()["dataset"+str(i)] = listToDataframe(e)
+		else:
+			globals()["replaceCmd"] = modifyReplace(i,e)
+
+def pprintErr(e):
+	exc_type, exc_obj, exc_tb = exc_info()
+	fname = 'AlphaSheets Python evaluator'
+	err = repr(e).replace("\'","").replace("'",'"')
+	pos = exc_tb.tb_lineno - 52 # subtract template lines
+	return {'errType': repr(exc_type), 'file': fname, 'position': pos, 'error': err}
+
+def pprintSql(res):
+	if isinstance(res, pd.DataFrame):
+		return pprint(res)
+	else: 
+		return res
+
+try:
+	setGlobals(["[\"NAME\",5,5,\"OFER\",\"TIM\"]"])
+	result = pprintSql(db('SELECT * FROM dataset0'))
+except Exception as e:
+	result = pprintErr(e)
