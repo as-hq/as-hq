c96e546b09996b148cf06837f2bebb0dcebcd2bd
anand <asrini@mit.edu>
2015-11-17 23:49:15 -0500

    bools and nulls work with json.dumps

--- server/eval_files/py/run.py
+++ server/eval_files/py/run.py
@@ -30,7 +30,7 @@ result = "error"
 try:
 	execfile(replFile)
 	os.chdir(os.getcwd()+"/static")
-	result = 12
+	result = 10+1
 	os.chdir('..')
 	figures=[manager.canvas.figure for manager in matplotlib._pylab_helpers.Gcf.get_all_fig_managers()]
 	if len(figures) > 0:
