1e56b7c3ecd769640319b860ef7cbddb11ba53a6
anand <asrini@mit.edu>
2015-11-17 20:15:32 -0500

    small graph db fix

--- graph-database/graph.cpp
+++ graph-database/graph.cpp
@@ -104,9 +104,15 @@ void DAG::updateDAG(DAG::Vertex toLoc, const DAG::VertexSet& fromLocs, bool addT
 
 	/* Loop over the new fromLocs and add to the forward adjacency list */
 	for (const auto& fl : fromLocs) {
+		// if Pointer reference, put an Index ref in the forward list, and put the Pointer ref in the backward list.
+		// then evalChain' triggers the recomputation of an expression with an @ reference in it correctly
+		// using getDescendants, then uses getAncestors on those descendants to get the actual Pointer reference.
+		toFromAdjList[toLoc].insert(fl);
+		if (fl[0] == 'P') {
+			fl[0] = 'I'; 
+		}
 		fromToAdjList[fl].insert(toLoc);
 	/* Replace toLoc entry in backwards adjacency list */
-		toFromAdjList[toLoc].insert(fl);
 	}
 
 }
