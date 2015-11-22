95e6ec9449c5d87753c82bb1f5e47217611ab282
Alex <zhukeepa@gmail.com>
2015-11-21 23:19:48 -0500

    fix repl template

--- server/eval_files/py/template_repl.py
+++ server/eval_files/py/template_repl.py
@@ -1,4 +1,5 @@
 import os
+curWd = os.getcwd()
 filename = os.getcwd() + "/eval_files/py/repl_record.py"
 execfile(filename)
 
@@ -15,15 +16,15 @@ from sys import exc_info
 from AS.iterable import ASIterable
 
 def arr(lst):
-	return ASIterable(lst)
+  return ASIterable(lst)
 result = ""
 try:
-	#CODE#
-	result = serialize(result)
-except Exception as e: 
-	exc_type, exc_obj, exc_tb = exc_info()
-	fname = 'AlphaSheets Python evaluator'
-	err = repr(e).replace("\'","").replace("'",'"')
-	pos = exc_tb.tb_lineno - 20 # subtract template lines
-	errJson = {'errType': repr(exc_type), 'file': fname, 'position': pos, 'error': err}
-	result = errJson
+  #CODE#
+  result = serialize(result)
+except Exception as e:
+  os.chdir(curWd)
+  print "exception"
+  exc_type, exc_obj, exc_tb = exc_info()
+  err = repr(e).replace("\'","").replace("'",'"')
+  errJson = {'tag': 'CellValue', 'cellValueType': 'Error', 'errorType': repr(exc_type), 'errorMsg': err}
+  result = json.dumps(errJson)
