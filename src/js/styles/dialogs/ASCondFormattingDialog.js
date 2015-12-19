d3fd438e602ab41fc72cc26b7715daa11cc8196c
Michael Gao <mgao4793@gmail.com>
2015-12-18 21:08:32 -0500

    Conditional formatting dialog actually looks nice now.

---
+++ src/js/styles/dialogs/ASCondFormattingDialog.js
@@ -0,0 +1,34 @@
+/* @flow */
+
+import type {
+  Dict
+} from '../../types/Base';
+
+export default {
+  ruleContainer(idx: number): Dict<any> {
+    let backgroundColor =
+      (idx % 2 === 0)
+        ? '#303030'
+        : '#424242'
+    return ({
+      paddingLeft: '16px',
+      paddingRight: '16px',
+      backgroundColor: backgroundColor
+    });
+  },
+
+  title: {
+    display: 'inline-block',
+    color: '#eeeeee',
+    lineHeight: '20px',
+    paddingTop: '0px',
+    marginTop: '18px',
+    marginBottom: '18px'
+  },
+
+  buttons: {
+    float: 'right',
+    marginTop: '10px',
+    marginBottom: '10px'
+  }
+}
