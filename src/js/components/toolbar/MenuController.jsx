119ccb260f48583eba6fa1a07f217a8e7d602227
alex <zhukeepa@gmail.com>
2015-12-31 03:49:22 -0500

    flowed MenuController.jsx

--- src/js/components/toolbar/MenuController.jsx
+++ src/js/components/toolbar/MenuController.jsx
@@ -13,35 +13,44 @@ It also adds listeners to stores by invoking ToolbarController, and makes sure t
 ToolbarStore.
 */
 
-export default React.createClass({
+import type {
+  ToolbarControlProps, 
+  MenuProps
+} from '../../types/Toolbar';
+
+import type {
+  NakedRange,
+  ASCell
+} from '../../types/Eval';
+
+type MenuControllerDefaultProps = {
+  toolbarWidth: number; 
+  toolbarHeight: number; 
+};
+
+type MenuControllerProps = {
+  toolbarComponent: React.Element; 
+  menuComponent: React.Element; 
+  setControlStateFromCell: (cell: ?ASCell) => void; 
+  propagateControlStateChange: (nextState: any, rng: NakedRange) =>  void; 
+  toolbarWidth: number;
+  toolbarHeight: number;
+  id: string;
+  onMenuShouldClose: () => void; 
+};
+
+type MenuControllerState = {};
+
+export default class MenuController
+  extends React.Component<MenuControllerDefaultProps, MenuControllerProps, MenuControllerState>
+{
+
+  constructor(props: MenuControllerProps) {
+    super(props);
+  }
 
   /*************************************************************************************************************************/
   // Prop and state methods
-
-  /* 
-    We need both components (menu and toolbar), 
-    Callbacks to pass the ToolbarController, 
-    Size data for styling,
-    The uid of the menu component for uniquess of dropdown,
-    A callback for when the menu needs to close due to uniquess of dropdown
-  */
-  propTypes: {
-    toolbarComponent: React.PropTypes.object.isRequired,
-    menuComponent: React.PropTypes.object.isRequired,
-    setControlStateFromCell: React.PropTypes.func.isRequired,
-    propagateControlStateChange: React.PropTypes.func.isRequired,
-    toolbarWidth: React.PropTypes.number,
-    toolbarHeight: React.PropTypes.number,
-    id: React.PropTypes.string.isRequired,
-    onMenuShouldClose: React.PropTypes.func.isRequired
-  },
-
-  getDefaultProps(){
-    return {
-      toolbarWidth: 100, 
-      toolbarHeight: 36
-    };
-  },
 
   /*************************************************************************************************************************/
   // Mounting
@@ -49,20 +58,20 @@ export default React.createClass({
 
   componentDidMount() {
     // After the initial render, inform the ToolbarStore of our existence
-    ToolbarStore.addChangeListener(this._onDropdownClicked);
+    ToolbarStore.addChangeListener(this._onDropdownClicked.bind(this));
     this._informStore();
-  },
+  }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps: MenuControllerProps, prevState: MenuControllerState) {
     // After a non-initial render, inform the ToolbarStore of our existence
     this._informStore();
-  },
+  }
 
   componentWillUnmount() {
     // Inform the ToolbarStore that we're gone
     this._informStore();
-    ToolbarStore.removeChangeListener(this._onDropdownClicked);
-  },
+    ToolbarStore.removeChangeListener(this._onDropdownClicked.bind(this));
+  }
 
   // Fire an action if the ToolbarStore has out-of-date info
   _informStore() {
@@ -77,11 +86,11 @@ export default React.createClass({
     } else {
       ToolbarActionCreator.click(this._menuVisible(), this.props.id);
     }
-  },
+  }
 
-  _menuVisible() {
+  _menuVisible(): boolean {
     return this.props.menuComponent != null;
-  },
+  }
 
   // When the store has a change event, some dropdown was clicked. If it's not this dropdown, and this dropdown is visible, 
   // inform the parent, which will update its state and then render the menu invisible.
@@ -90,13 +99,12 @@ export default React.createClass({
     if (lastClickedId !== this.props.id && this._menuVisible()) {
       this.props.onMenuShouldClose();
     }
-  },
-
+  }
 
   /*************************************************************************************************************************/
   // Styling and rendering
 
-  getStyles() {
+  getStyles(): any {
     let width = this.props.toolbarWidth;
     let height = this.props.toolbarHeight;
     return {
@@ -114,9 +122,9 @@ export default React.createClass({
         zIndex: 50 // needed to display over the editor/sheet
       },
     };
-  },
+  }
 
-  render() {
+  render(): React.Element {
     let {menuStyle} = this.getStyles();
     let toolbarComponentWithMenu = 
       <span>
@@ -133,11 +141,27 @@ export default React.createClass({
           control={toolbarComponentWithMenu}/>
     );
   }
-});
+}
 
+/* 
+  We need both components (menu and toolbar), 
+  Callbacks to pass the ToolbarController, 
+  Size data for styling,
+  The uid of the menu component for uniquess of dropdown,
+  A callback for when the menu needs to close due to uniquess of dropdown
+*/
+MenuController.propTypes =  {
+  toolbarComponent: React.PropTypes.object.isRequired,
+  menuComponent: React.PropTypes.object.isRequired,
+  setControlStateFromCell: React.PropTypes.func.isRequired,
+  propagateControlStateChange: React.PropTypes.func.isRequired,
+  toolbarWidth: React.PropTypes.number,
+  toolbarHeight: React.PropTypes.number,
+  id: React.PropTypes.string.isRequired,
+  onMenuShouldClose: React.PropTypes.func.isRequired
+};
 
-
-
-
-
-
+MenuController.defaultTypes = {
+  toolbarWidth: 100, 
+  toolbarHeight: 36
+};
