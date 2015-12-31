49deaf8aad933243b2f5ffd189cdf927ce5851eb
alex <zhukeepa@gmail.com>
2015-12-31 03:25:13 -0500

    flow FontSizePicker

--- src/js/components/toolbar/FontSizePicker.jsx
+++ src/js/components/toolbar/FontSizePicker.jsx
@@ -1,3 +1,5 @@
+/* @flow */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Styles, FontIcon} from 'material-ui';
@@ -6,53 +8,77 @@ import ToolbarController from './ToolbarController.jsx';
 import ToolbarTextField from './ToolbarTextField.jsx';
 import GenerateToolbarMenu from './GenerateToolbarMenu.jsx';
 
+import type {
+  ToolbarControlProps, 
+  MenuProps
+} from '../../types/Toolbar';
 
-export default React.createClass({
+import type {
+  NakedRange,
+  ASCell
+} from '../../types/Eval';
+
+type FontSizePickerDefaultProps = {};
+
+type FontSizePickerProps = {};
+
+type FontSizePickerState = {};
+
+export default class FontSizePicker
+  extends React.Component<FontSizePickerDefaultProps, FontSizePickerProps, FontSizePickerState>
+{
 
   /*************************************************************************************************************************/
   // Sub-component generation
 
-  fontSizes: [6, 7, 8, 9, 10, 11, 12, 14, 18, 24, 36],
+  fontSizes: Array<number>; 
 
-  getMenuProps() {
+  constructor(props: FontSizePickerProps) {
+    super(props);
+
+    this.fontSizes = [6, 7, 8, 9, 10, 11, 12, 14, 18, 24, 36];
+  }
+
+
+  getMenuProps(): Array<MenuProps> {
     return this.fontSizes.map((size) => {
       return {tag: 'MenuItem', primaryText: `${size}`, value: `${size}`};
     });
-  },
+  }
 
-  toolbarControlProps() {
+  toolbarControlProps(): ToolbarControlProps {
     return {
       tooltip: "Font size",
       displayValue: '10',
       showTooltip: true,
       width: 45
     };
-  },
+  }
 
   /*************************************************************************************************************************/
   // Helper methods to pass to generator
 
   // When the active cell changes to a new cell, get the new menu value that should be selected/checked 
-  _getMenuValueFromCell(cell) {
+  _getMenuValueFromCell(cell: ASCell): string {
     return "10"; // TODO: something like cell.cellProps.font.fontSize
-  },
+  }
 
-  _propagateControlStateChange(nextValue, rng) {
+  _propagateControlStateChange(nextValue: string, rng: NakedRange) {
     // TODO: case on value, call some API function
     console.log("Propagating state change to backend: " + nextValue);
-  },
+  }
 
   // Update the toolbar control props given a the menu visibility, menuValue, and current toolbarProps.
-  _toolbarControlPropTransform(menuVisible, menuValue, toolbarControlProps) {
+  _toolbarControlPropTransform(menuVisible: boolean, menuValue: string, toolbarControlProps: ToolbarControlProps): ToolbarControlProps {
     toolbarControlProps.showTooltip = !menuVisible;
     toolbarControlProps.displayValue = menuValue;
     return toolbarControlProps;
-  },
+  }
 
   /*************************************************************************************************************************/
   //Render
 
-  render() {
+  render(): React.Element {
     let ButtonWithMenu = GenerateToolbarMenu(ToolbarTextField);
     return (
       <ButtonWithMenu
@@ -68,5 +94,4 @@ export default React.createClass({
     );
   }
 
-});
-
+}
--- src/js/styles/menu-bar/ASMenuBar.js
+++ src/js/styles/menu-bar/ASMenuBar.js
@@ -1,5 +1,3 @@
-/* @flow */
-
 export default {
   root: {
     width: '100%',
