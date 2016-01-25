5e69c04885009dfc7358f5f8972e020b1767f923
alex <zhukeepa@gmail.com>
2016-01-25 02:24:52 -0500

    filter bars, cond format rules, and descriptors when broadcasting, rather than just cells
    Summary: .
    
    Test Plan:
    Previously, this would happen:
    - open up two sheets.
    - Add a conditional format rule in sheet 1.
    - DO NOT RELOAD sheet 2, and look at the list of conditional format rules.
    You would see the rule added in sheet 1 inside of sheet 2. This should no longer happen with this commit.
    
    Reviewers: anand, RITESH, timchu
    
    Reviewed By: timchu
    
    Differential Revision: http://18.102.225.27:11000/D50

--- backend/server/src/AS/Types/Updates.hs
+++ backend/server/src/AS/Types/Updates.hs
@@ -16,14 +16,25 @@ data Diff a = Diff { afterVals :: [a], beforeVals :: [a] } deriving (Show, Read,
 flipDiff :: Diff a -> Diff a
 flipDiff (Diff x y) = Diff y x
 
--- #expert would like to somehow ensure that a is an instance of HasKey and b is KeyType a. 
+-- #needsrefactor make this a GADT to restrict b to be of type Keytype a
+-- | To apply an update, we delete the values at oldKeys, and then add the newVals in. 
+-- For example if oldKeys is (1,1), (1,2), and (1,3), and I have a new cell CELL (that's
+-- a variable name) at (1,1), applying this update means deleting what's at (1,1), (1,2), and (1,3) 
+-- and adding CELL to (1,1). This is used both on the backend to update the DB and on the frontend
+-- to update the spreadsheet presented to the user. 
 data Update a b = Update { newVals :: [a], oldKeys :: [b] } deriving (Show, Read, Eq, Generic)
 -- You could write a constructor yourself (not sure if compiles) #anand
 --mkUpdate :: (HasKey a, Eq (KeyType b)) => [a] -> [KeyType b] -> Update a (KeyType b)
---mkUpdate as bs = Update {newVals = as, oldKeys = bs}
+--mkUpdate as bs = Update {newVals = as, oldKeys = bs}1
 
 diffToUpdate :: (HasKey a, Eq (KeyType a)) => Diff a -> Update a (KeyType a)
 diffToUpdate (Diff after before) = Update after ((map key before) \\ (map key after))
+
+filterUpdateByKey :: (HasKey a, Eq (KeyType a)) => ((KeyType a) -> Bool) -> Update a (KeyType a) -> Update a (KeyType a)
+filterUpdateByKey f (Update newVals oldKeys) = Update newVals' oldKeys' 
+  where 
+    newVals' = filter (f . key) newVals
+    oldKeys' = filter f oldKeys
 
 -- Assumes beforeVals is a subset of the thing you're taking a diff of. 
 addValue :: (HasKey a, Eq (KeyType a)) => Update a (KeyType a) -> a -> Update a (KeyType a)
