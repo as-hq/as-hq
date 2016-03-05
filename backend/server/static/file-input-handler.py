bcacee442ad8ba3461f2ec7da63aa4a46a44f63d
RiteshRN <riteshr@mit.edu>
2016-03-05 01:23:47 -0800

    changed docker environment, deleted frontendAddress from file input handler

--- backend/server/static/file-input-handler.py
+++ backend/server/static/file-input-handler.py
@@ -3,14 +3,9 @@ import os
 import cgi
 import json
 
-defaultHost = '18.102.225.27'
-defaultFrontendPort = 8080
-defaultFileHandlingPort = 9000
-
 # Very simple server that saves to the current folder (static)
 # Handles file uploads, should also deal with multiple files/parts
 class FileInputHandler(BaseHTTPRequestHandler):
-  frontendAddress = None # required to be set
 
   def do_POST(self):
     print "got post request"
@@ -21,12 +16,10 @@ class FileInputHandler(BaseHTTPRequestHandler):
         fh.close()
     # Need to deal with CORS
     self.send_response(200)
-    self.send_header('Access-Control-Allow-Origin', self.frontendAddress)
+    self.send_header('Access-Control-Allow-Origin', '*')
     self.end_headers()
    
 if __name__ == '__main__':
-
-  settings = None
 
   f = open('../../Environment.json', 'r')
   settings = json.loads(f.read())
@@ -37,11 +30,7 @@ if __name__ == '__main__':
 
   fileInputHandlerPort = settings['fileInputHandlerPort']
   print("Attaching to port '" + str(fileInputHandlerPort) + "'\n")
-
-  frontendPort = settings['frontendPort']
-  print("Using frontend port '" + str(frontendPort) + "'\n")
   
-  FileInputHandler.frontendAddress = 'http://' + host + ':' + str(frontendPort)
   server_address = (host, fileInputHandlerPort)
   httpd = HTTPServer(server_address, FileInputHandler)
   print('http server is running on address: ' + str(server_address))
