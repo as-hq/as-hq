bb1676722b5af4820eef2aacef83bd8b662d2817
anand <asrini@mit.edu>
2016-03-05 01:46:05 -0800

    fixed router server

--- deployment/router.py
+++ deployment/router.py
@@ -4,12 +4,17 @@ import cgi
 import json
 import uuid
 import random
-import json
+
+router_address          = ('0.0.0.0', 10000)
+
+default_backend_port    = 20000
+default_fileinput_port  = 30000
+default_static_port     = 40000
 
 # this class should be treated as immutable.
 # all instance methods return new objects and do not mutate the underlying.
 class ASInstance(object):
-  def __init__(backend_port, fileinput_port, static_port):
+  def __init__(self, backend_port, fileinput_port, static_port):
     self.backend_port = backend_port
     self.fileinput_port = fileinput_port
     self.static_port = static_port
@@ -23,32 +28,35 @@ class ASInstance(object):
 
   # immutable
   def toJSON(self):
-    return {backend_port: self.backend_port, \
-            fileinput_port: self.fileinput_port \
-            static_port: self.static_port \
-            name: self.name}
+    return {'backend_port': self.backend_port, \
+            'fileinput_port': self.fileinput_port, \
+            'static_port': self.static_port, \
+            'name': self.name}
 
-class Router(BaseHTTPRequestHandler):
-  def initInstances(self):
-    # track the last created instance (even if it is eventually deleted)
-    # so that we never use the same ports twice
-    self.last_created_instance = None
-    # a mapping from instance ID to instance
-    self.instances = {}
+class ASRouter(BaseHTTPRequestHandler):
+  # track the last created instance (even if it is eventually deleted)
+  # so that we never use the same ports twice
+  last_created_instance = None
+  # a mapping from instance ID to instance
+  instances = {}
 
   # returns ASInstance
   def createInstance(self):
-    if self.last_created_instance is not None:
-      newInstance = self.last_created_instance.incrementPorts()
+    if ASRouter.last_created_instance is not None:
+      newInstance = ASRouter.last_created_instance.incrementPorts()
     else:
-      newInstance = ASInstance(backend_port=20000, fileinput_port=30000, static_port=40000)
+      newInstance = ASInstance(backend_port = default_backend_port, \
+                               fileinput_port = default_fileinput_port, \
+                               static_port = default_static_port)
+      ASRouter.last_created_instance = newInstance
 
-    self.instances[newInstance.name] = newInstance
-    self.last_created_instance = newInstance
+    ASRouter.instances[newInstance.name] = newInstance
+    ASRouter.last_created_instance = newInstance
     return newInstance
 
   def destroyInstance(self, name): 
-    del self.instances[name]
+    if (name in ASRouter.instances):
+      del ASRouter.instances[name]
 
   # allow cross-origin from all origins
   def do_OPTIONS(self):
@@ -61,15 +69,15 @@ class Router(BaseHTTPRequestHandler):
 
   # the route clients will use to request a backend instance to connect to 
   def do_GET(self):
-    resp = random.choice(self.instances.keys()).toJSON()
+    resp = random.choice(self.instances.values()).toJSON()
     self.send_response(200)
     self.send_header('instance', json.dumps(resp))
 
-  # the route spinup and spindown scripts will use to create and destroy instances
+  # the container spinup and spindown scripts will use to create and destroy instances
   def do_POST(self):
     print "got post request"
     content_len = int(self.headers.getheader('content-length', 0))
-    post_body = self.rfile.read(content_len)
+    post_body = json.loads(self.rfile.read(content_len))
 
     if post_body['action'] == 'create':
       resp = self.createInstance().toJSON()
@@ -84,3 +92,8 @@ class Router(BaseHTTPRequestHandler):
     else:
       self.send_response(400)
       print "ERROR: received action other than create or destroy"
+
+if __name__ == '__main__':
+  httpd = HTTPServer(router_address, ASRouter)
+  print('Router running on address: ' + str(router_address))
+  httpd.serve_forever()
