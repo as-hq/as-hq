b81793bc29f23db68ef9de9924afcb54144ad990
RiteshRN <riteshr@mit.edu>
2016-03-07 19:45:12 -0800

    dockerfile changes

--- deployment/startup.sh
+++ deployment/startup.sh
@@ -4,17 +4,9 @@
 /etc/init.d/redis-server stop
 /etc/init.d/redis-server start
 
-# Kill running processes
-fuser -k 80/tcp
-fuser -k 8000/tcp
-fuser -k 20000/tcp 
-fuser -k 9000/tcp
-fuser -k 5000/tcp
-fuser -k 5555/tcp
-
 # Run the Haskell, C++, and Python executables. There is one frontend instance on the demo server, 
 # so we don't need to build the frontend inside the container
-cd ../home/alphasheets
+cd /home/alphasheets
 cd graph
 ./server &
 cd ../pykernel/server
@@ -24,7 +16,7 @@ cd file-input-handler/file-input-handler
 ./file-input-handler &
 cd ../..
 cd backend
-stack exec -- /home/alphasheets/backend/alphasheets-exe 
+/home/alphasheets/backend/alphasheets-exe 
 
 
 
