e7182723227c6bdae5ff6cf132d115c2007db5f8
anand <asrini@mit.edu>
2016-03-08 19:45:16 -0800

    fix filepatch in script

--- deployment/redeploy.sh
+++ deployment/redeploy.sh
@@ -9,7 +9,7 @@ curl -H "Content-Type: application/json" -X POST \
      -d '{"action":"redeploy_all"}' http://localhost:10000
 
 # redeploy frontend
-cd ..
+cd ../..
 cp -r frontend /www/alphasheets
 nginx -s reload
 
