48c25ec20ead9c186b436097b2e88fbc52330486
Michael Gao <mgao4793@gmail.com>
2016-03-11 17:50:33 -0800

    Rate limiter
    Summary: Added a 100ms rate limit to evaluation on the frontend, in APIActionCreators
    
    Test Plan: Try evaluating by holding down Ctrl-Enter. The cell should flash every 100ms, but the backend shouldn't crash and it shouldn't flash continuously.
    
    Reviewers: zhukeepa
    
    Differential Revision: https://alphasheets.phacility.com/D175

--- frontend/src/js/actions/APIActionCreators.js
+++ frontend/src/js/actions/APIActionCreators.js
@@ -8,15 +8,24 @@ import Dispatcher from '../Dispatcher';
 import GridStore from '../stores/ASGridStore';
 import ExpressionStore from '../stores/ASExpressionStore';
 
+const EVAL_RATE_LIMIT = 100;
+
+let lastEval = 0;
+
 export default {
   evaluate(offset: Offset, providedExpression?: string) {
-    const origin = ExpressionStore.getTextboxPosition();
-    const expression = providedExpression || ExpressionStore.getExpression();
-    const language = ExpressionStore.getLanguage();
-    Dispatcher.dispatch({
-      _type: 'API_EVALUATE',
-      offset
-    });
-    API.evaluate(origin, expression, language);
+    const curTime = new Date().getTime();
+    if (curTime - lastEval >= EVAL_RATE_LIMIT) {
+      lastEval = curTime;
+      
+      const origin = ExpressionStore.getTextboxPosition();
+      const expression = providedExpression || ExpressionStore.getExpression();
+      const language = ExpressionStore.getLanguage();
+      Dispatcher.dispatch({
+        _type: 'API_EVALUATE',
+        offset
+      });
+      API.evaluate(origin, expression, language);
+    }
   }
 }
