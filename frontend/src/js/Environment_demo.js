f888a19cf3f1edee3a87f37a04d66ac808fc5a0a
anand <asrini@mit.edu>
2016-03-12 22:39:36 -0800

    updated demo Environment

--- frontend/src/js/Environment_demo.js
+++ frontend/src/js/Environment_demo.js
@@ -2,4 +2,8 @@ export default {
 	BACKEND_WS_PORT: '5200',
 	BACKEND_STATIC_PORT: '8200',
 	BACKEND_IMPORT_PORT: '9200'
+	isRemote: true,
+	noRouter: true,
+	REMOTE_HOST: 'stable.alphasheets.com',
 }
+
