148d8fda893e23177f600968a6cad0528172ccd1
anand <asrini@mit.edu>
2016-03-13 04:14:09 -0700

    fixed syntax error in Environment_demo

--- frontend/src/js/Environment_demo.js
+++ frontend/src/js/Environment_demo.js
@@ -1,7 +1,7 @@
 export default {
 	BACKEND_WS_PORT: '5200',
 	BACKEND_STATIC_PORT: '8200',
-	BACKEND_IMPORT_PORT: '9200'
+	BACKEND_IMPORT_PORT: '9200',
 	isRemote: true,
 	noRouter: true,
 	REMOTE_HOST: 'stable.alphasheets.com',
