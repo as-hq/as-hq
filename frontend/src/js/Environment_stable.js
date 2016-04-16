6d9e2b905b0d791013fa05a79da2effd07fb07b9
anand <asrini@mit.edu>
2016-04-15 18:49:59 -0700

    correct host for stable

--- frontend/src/js/Environment_stable.js
+++ frontend/src/js/Environment_stable.js
@@ -2,6 +2,6 @@ export default {
 	isRemote: true,
 	noRouter: true,
 	logActions: false,
-	REMOTE_HOST: 'stable.alphasheets.com',
+	REMOTE_HOST: 'stable2.alphasheets.com',
 	BACKEND_WS_PORT: 5000
 }
