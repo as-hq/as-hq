0929dccd2c8d6380c60d65e3caec93fddcd27bb3
anand <asrini@mit.edu>
2016-04-28 11:56:28 -0700

    initialize dev server script for tweag

--- scripts/deploy_all_dev.sh
+++ scripts/deploy_all_dev.sh
@@ -2,11 +2,15 @@
 
 tmux ls | grep : | cut -d. -f1 | awk '{print substr($1, 0, length($1)-1)}' | tmux kill-session 
 
-tmux new -s "redis" -d "redis-server"
-tmux new -s "graph_db" -d "cd ../backend/graph-database; ./server"
-tmux new -s "python_kernel" -d "cd ../backend/pykernel; python server.py"
-tmux new -s "backend" -d "cd ../backend/server; stack exec alphasheets-exe"
-tmux new -s "r_kernel" -d "cd ../backend/server; stack exec rkernel-exe"
-tmux new -s "static_server" -d "cd ../backend/server/static; python -m SimpleHTTPServer"
-tmux new -s "file_import" -d "cd ../backend/server/static; python file-input-handler.py"
-tmux new -s "frontend" -d "cd ../frontend; npm start"
+cd backend/graph-database 
+tmux new -s "graph_db" -d "./server"
+cd ../pykernel
+tmux new -s "python_kernel" -d "python server.py"
+cd ../server
+tmux new -s "backend" -d "stack exec alphasheets-exe"
+tmux new -s "r_kernel" -d "stack exec rkernel-exe"
+cd static
+tmux new -s "static_server" -d "python -m SimpleHTTPServer"
+tmux new -s "file_import" -d "python file-input-handler.py"
+cd ~/codebase/frontend
+tmux new -s "frontend" -d "npm start"
