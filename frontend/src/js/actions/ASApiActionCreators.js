9bc4727591d98edfcd3b055ffcaa1e55b4479b89
riteshr <asrini@mit.edu>
2016-05-02 16:02:31 -0700

    remove remote condition from user logins (sorry)

--- frontend/src/js/actions/ASApiActionCreators.js
+++ frontend/src/js/actions/ASApiActionCreators.js
@@ -371,8 +371,8 @@ pws.whenReady(() => {
         const {authUserId, defaultSheetId} = action;
         LoginActions.onLoginSuccess(authUserId, defaultSheetId);
         const host = Constants.getFrontendHost();
-        // Log a login success to slack if it's remote, not a dev/public, and not master
-        if (Constants.isRemote && host !== 'master.alphasheets.com' && !LoginStore.userIsDev() && !LoginStore.isPublicLogin()) {
+        // Log a login success to slack if it's  not a dev/public, and not master
+        if (host !== 'master.alphasheets.com' && !LoginStore.userIsDev() && !LoginStore.isPublicLogin()) {
           const slackMsg = SheetStateStore.getSheetLink(false) + '\n' + LoginStore.getUserId();
           logSlack(slackMsg, '#userlogins');
         }
