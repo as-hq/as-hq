f12cee5ded5d67dbdba24992e0e6bae8245d9228
Scott Powell <scott.powell@toptal.com>
2016-05-15 23:13:47 -0700

    cleanup deploy script from main branch

--- deployment/deploy/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Setup
-echo Testing, testing, 1 2 3
-apt-get install something_that_doesnt_exist
-
-# Segment 2
-echo Starting segment two...
-apt-cache search gdbg
--- deployment/deploy/libsendao/senlist.h
+++
@@ -1,4 +0,0 @@
-#ifndef _SENDAO_LIST_H
-#define _SENDAO_LIST_H
-
-#endif
