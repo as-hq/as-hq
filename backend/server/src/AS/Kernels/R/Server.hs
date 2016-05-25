8e813ebf5da1487f39093eca3bb809b3f1c17838
riteshr <asrini@mit.edu>
2016-05-25 00:08:59 -0700

    additional r packages for twitter api

--- backend/server/src/AS/Kernels/R/Server.hs
+++ backend/server/src/AS/Kernels/R/Server.hs
@@ -99,6 +99,10 @@ runServer url_clients numInitialWorkers = do
     [r|
       library("rjson")
       library("ggplot2")
+      library("devtools")
+      library("twitteR")
+      library("bit64")
+      library("httr")
     |] 
 
    -- Close resources cleanly upon Ctrl+C
